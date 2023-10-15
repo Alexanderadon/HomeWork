51a8d85a687486e8597c96c3df85dbb016f07582
Alexander <63007496+Alexanderadon@users.noreply.github.com>
2023-10-15 21:11:05 +0600

    Delete widgets directory

--- widgets/Paths/index.jsx
+++
@@ -1,9 +0,0 @@
-export const Paths = {
-  home: "/",
-  lectureOne: "/lectureOne",
-  lectureTwo: "/lectureTwo",
-  quiz: "/quiz",
-  generatorAi: "/generatorAi",
-  lectureFive: "/lectureFive",
-  lectureSix: "/lectureSix",
-}
